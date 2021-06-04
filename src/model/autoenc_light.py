c64bc4514742a320fbabd50a504b0b9514467400
Marek <marekbf3@gmail.com>
2021-06-04 15:36:03 +0200

    default options from best model, batch norm + init options

--- src/model/autoenc_light.py
+++ src/model/autoenc_light.py
@@ -4,6 +4,7 @@ from tensorflow.python.keras.layers.core import Reshape
 from data.default_loader import get_preprocessing_layers
 import copy
 from .param_utils import get_array_from_params_split
+from hydra.experimental import compose, initialize, initialize_config_dir
 
 class Autoenc_light(tf.keras.Model):
     def __init__(self, ds_mean = None, params: dict = {}):
@@ -21,26 +22,44 @@ class Autoenc_light(tf.keras.Model):
 
         self.preprocess = get_preprocessing_layers(ds_mean)
 
-        self.encoder = tf.keras.Sequential([
+        encoder_layers = [
                     InputLayer(input_shape=(96, 96, 3)),
-                    Conv2D(filters=params['e_filters'][0], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same'),
-                    Conv2D(filters=params['e_filters'][1], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same'),
-                    Conv2D(filters=params['e_filters'][2], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same'),
-                    Conv2D(filters=params['e_filters'][3], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same'),
-                    Conv2D(filters=params['e_filters'][4], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same'),
+                    Conv2D(filters=params['e_filters'][0], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same',
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
+                    Conv2D(filters=params['e_filters'][1], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same',
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
+                    Conv2D(filters=params['e_filters'][2], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same',
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
+                    Conv2D(filters=params['e_filters'][3], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same',
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
+                    Conv2D(filters=params['e_filters'][4], kernel_size=3, strides=(1, 1), activation=params['e_activation'], padding='same',
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
                     
-                    Conv2D(filters=params['e_filters'][5], kernel_size=3, strides=(2, 2), activation=params['e_activation']),
-                    Conv2D(filters=params['e_filters'][6], kernel_size=3, strides=(2, 2), activation=params['e_activation']),
+                    Conv2D(filters=params['e_filters'][5], kernel_size=3, strides=(2, 2), activation=params['e_activation'],
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
+                    Conv2D(filters=params['e_filters'][6], kernel_size=3, strides=(2, 2), activation=params['e_activation'],
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
 
-                    Conv2D(filters=params['e_filters'][7], kernel_size=5, strides=(2, 2), activation=params['e_activation']),
+                    Conv2D(filters=params['e_filters'][7], kernel_size=5, strides=(2, 2), activation=params['e_activation'],
+                    kernel_initializer=params['e_initializer'], bias_initializer=params['e_initializer']),
 
                     Reshape((10*10*params['e_filters'][7],)),
                     Dense(10*10*params['e_filters'][7], activation='linear'),
                     Dense(6*6*params['e_filters'][7], activation='linear'),
                     Reshape((6,6,params['e_filters'][7],)),
 
-                    Conv2D(filters=params['e_filters'][8], kernel_size=3, strides=(1, 1), activation=params['e_h_activation'], padding='same'),
-                ])
+                    Conv2D(filters=params['e_filters'][8], kernel_size=3, strides=(1, 1), activation=params['e_h_activation'], padding='same',
+                    kernel_initializer=params['e_h_initializer'], bias_initializer=params['e_h_initializer']),
+                ]
+        if params['batch_norm'] == True:
+            i = 2
+            while i < 18:
+                encoder_layers.insert(i, BatchNormalization())
+                i += 2
+            encoder_layers.insert(20, BatchNormalization())
+            
+
+        self.encoder = tf.keras.Sequential(encoder_layers)
         #self.encoder_preview = tf.keras.Model(inputs=self.encoder.input, outputs=self.encoder.get_layer("latent").output)
 
         #(1,1,channels) tensor with mean of each channel -
@@ -52,9 +71,12 @@ class Autoenc_light(tf.keras.Model):
             [
                 InputLayer(input_shape=(dec_shape[0],dec_shape[1],dec_shape[2])),
   
-                Conv2DTranspose(params['d_filters'][0], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same'),
-                Conv2DTranspose(params['d_filters'][1], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same'),
-                Conv2DTranspose(params['d_filters'][2], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same'),
+                Conv2DTranspose(params['d_filters'][0], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same',
+                    kernel_initializer=params['d_initializer'], bias_initializer=params['d_initializer']),
+                Conv2DTranspose(params['d_filters'][1], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same',
+                    kernel_initializer=params['d_initializer'], bias_initializer=params['d_initializer']),
+                Conv2DTranspose(params['d_filters'][2], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same',
+                    kernel_initializer=params['d_initializer'], bias_initializer=params['d_initializer']),
 
                 Conv2DTranspose(3, kernel_size=5, strides=(2,2), activation='sigmoid', padding='same')
             ]
@@ -63,9 +85,12 @@ class Autoenc_light(tf.keras.Model):
             [
                 InputLayer(input_shape=(dec_shape[0],dec_shape[1],dec_shape[2])),
   
-                Conv2DTranspose(params['d_filters'][0], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same'),
-                Conv2DTranspose(params['d_filters'][1], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same'),
-                Conv2DTranspose(params['d_filters'][2], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same'),
+                Conv2DTranspose(params['d_filters'][0], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same',
+                    kernel_initializer=params['d_initializer'], bias_initializer=params['d_initializer']),
+                Conv2DTranspose(params['d_filters'][1], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same',
+                    kernel_initializer=params['d_initializer'], bias_initializer=params['d_initializer']),
+                Conv2DTranspose(params['d_filters'][2], kernel_size=3, strides=(2,2), activation=params['d_activation'], padding='same',
+                    kernel_initializer=params['d_initializer'], bias_initializer=params['d_initializer']),
 
                 Conv2DTranspose(3, kernel_size=5, strides=(2,2), activation='sigmoid', padding='same')
             ]
@@ -92,7 +117,9 @@ class Autoenc_light(tf.keras.Model):
 
 
 if __name__ == '__main__':
-    model = Autoenc_light()
+    with initialize(config_path='../conf'):
+        cfg = compose(config_name="config", overrides=['model=autoenc_light'])
+    model = Autoenc_light(params=cfg['model'])
     model.build((1,96,96,3))
     model.encoder.summary()
     model.decoder1.summary()
